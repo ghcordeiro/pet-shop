a7ba139b5090c9386ab2cc3de4ab102548cefad0
Guilherme Cordeiro <guilherme.helly@gmail.com>
2020-04-08 21:57:54 -0300

    Ajustes nos layouts e modal de deleção

--- src/main/java/br/unipar/petshop/modelo/Cliente.java
+++ src/main/java/br/unipar/petshop/modelo/Cliente.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cliente {
 	
 	private Integer id;
+
+	@NotNull(message = "Nome não pode ser nulo")
+	@Size(min = 3, message = "Nome deve ter pelo menos 3 caracteres")
 	private String nome;
+
+	@NotNull(message = "CPF não pode ser nulo")
+	@Size(min = 3, max = 11, message = "CPF deve ter 11 caracteres")
 	private String cpf;
 	
 }
